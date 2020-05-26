65c348d3eb1ff2e073707ddd25eba568b7b31039
Felipe Viana <flpnasa@gmail.com>
2020-05-26 18:50:45 -0300

    Removendo linhas sem uso

--- cep-api.php
+++ cep-api.php
@@ -12,16 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Incluir arquivos JS necessarios
 add_action('wp_enqueue_scripts','api_integrate_init');
 
-function api_integrate_init() {    
-    //wp_enqueue_script( 'bootstrap-js', plugins_url() . '/api-integrate/js/bootstrap.min.js', array(), '1.0.0', true );
+function api_integrate_init() {        
     wp_enqueue_script( 'mask', plugins_url() . '/cep-api/js/jquery.mask.min.js', array(), '1.0.0', true );
     wp_enqueue_script( 'functionalities', plugins_url() . '/cep-api/js/functionalities.js', array(), '1.0.0', true );
 }
-
-/*
-// Inclui o modal no footer
-add_action('wp_footer', 'modal_form');
-function modal_form() {
-    include 'form-login.php';
-}
-*/
